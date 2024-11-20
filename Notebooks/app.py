863ed222fc2d15cd5355800f0ee46d6ea26e376d
Y@y@ <brainy333@gmail.com>
2024-11-20 21:51:09 +0000

    app

--- Notebooks/app.py
+++ Notebooks/app.py
@@ -3,13 +3,19 @@ import plotly.express as px
 import streamlit as st 
 
 car_data = pd.read_csv('/workspaces/Data-analyst-Portafolio/Notebooks/vehicles_us.csv')
-hist_button = st.button('Construir Histograma')
+st.header('Proyecto 7 - Herramientas de Desarrollo de Software')
 
-if hist_button:
+st.write('Vista Previa del Conjunto de Datos')
+st.head(car_data.head())
+
+if st.checkbox('Generar Histograma'):
     st.write('Creación de un histograma para el conjunto CSV')
-    
     fig = px.histogram(car_data, x = 'odometer')
-    
     st.plotly_chart(fig, use_container_width=True)
 
+  if st.checkbox('Generar Gráfico de Dispersión'):
+    st.write('Creación de un Gráfico de Dispersión para el conjunto CSV')
     
+    fig = px.scatter(car_data, x = 'odometer', y = 'price')
+    
+    st.plotly_chart(fig)  
